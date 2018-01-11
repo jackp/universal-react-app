9bd6061e6ff97fcf1e85281be4ce47de8df623eb
Jackson Parker <jack@rethinq.io>
2018-01-11 09:09:09 -0500

    [setup] Move app entry points into their own folders

--- ios/hardly/AppDelegate.m
+++ ios/hardly/AppDelegate.m
@@ -18,7 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"ios/index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"hardly"
